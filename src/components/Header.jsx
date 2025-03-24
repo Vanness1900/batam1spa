445031b64fe1542a03443bbc9dd8cac1e7cd9747
Vanness1900 <vanvic.yang@gmail.com>
2025-03-24 18:36:17 +0800

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import HeaderLogo from '../assets/White_Logotype_Left_150DPI.png'
+
+function Header() {
+    return (
+        <>
+        <img src={HeaderLogo} />
+        </>
+    );
+}
+
+export default Header
